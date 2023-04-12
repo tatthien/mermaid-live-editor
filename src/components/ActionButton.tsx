2673d4b36a8978098bf79824d38769c788761e95
Thien Nguyen <tatthien.contact@gmail.com>
2023-04-12 20:03:06 +0700

    style: change primary button bg color

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -16,10 +16,10 @@ export const ActionButton = (props: {
   const variant = props.variant ?? 'primary'
 
   const commonClasses =
-    'inline-flex items-center justify-center gap-1.5 rounded border text-sm font-medium shadow-sm transition focus:outline-none focus:ring focus:ring-offset-1'
+    'inline-flex items-center justify-center gap-1.5 rounded border text-sm font-medium shadow-sm transition focus:outline-none focus:ring'
   const variantClasses =
     variant === 'primary'
-      ? 'bg-pink-500 text-white border-pink-600 hover:bg-pink-400 focus:ring-pink-400'
+      ? 'bg-slate-900 text-white hover:bg-slate-700 focus:ring-slate-400'
       : 'bg-white text-slate-600 border-slate-200 hover:bg-slate-50 focus:ring-slate-300'
   const disabledClasses = disabled ? 'opacity-40 cursor-not-allowed' : 'opacity-100 cursor-pointer'
   const spacingClasses = displayText ? 'px-1.5 py-1' : 'p-0.5'
